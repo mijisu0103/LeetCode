877a1f6f89c693d43b2ec8906ea024f1448060e0
Jisu Park <86939562+mijisu0103@users.noreply.github.com>
2025-10-17 20:35:08 +0900

    Time: 7 ms (95.99%), Space: 18.4 MB (81.29%) - LeetHub

--- LeetCode/Medium/1169-invalid-transactions/1169-invalid-transactions.py
+++ LeetCode/Medium/1169-invalid-transactions/1169-invalid-transactions.py
@@ -1,34 +1,25 @@
 from collections import defaultdict
 
 class Solution:
-    def invalidTransactions(self, transactions: List[str]) -> List[str]:
-        person_transactions = defaultdict(list)
-        invalid_transactions = []
+    def invalidTransactions(self, transactions: list[str]) -> list[str]:
+        
+        ans = []
+        nameToTrans = defaultdict(list)
 
-        for transaction in transactions:
-            name, time, amount, city = transaction.split(',')
+        for t in transactions:
+            name, time, amount, city = t.split(',')
             time, amount = int(time), int(amount)
-            person_transactions[name].append((time, amount, city, transaction))
+            nameToTrans[name].append({'time': time, 'city': city})
 
-        for name, trans_list in person_transactions.items():
-            trans_list.sort()
-
-            for i in range(len(trans_list)):
-                time_i, amount_i, city_i, trans_i = trans_list[i]
-                
-                # Check if the transaction amount exceeds $1000
-                if amount_i > 1000:
-                    invalid_transactions.append(trans_i)
-
-                # Check for transactions within 60 minutes in different cities
-                for j in range(i + 1, len(trans_list)):
-                    time_j, _, city_j, trans_j = trans_list[j]
-                    
-                    if time_j - time_i > 60:
+        for t in transactions:
+            name, time, amount, city = t.split(',')
+            time, amount = int(time), int(amount)
+            if amount > 1000:
+                ans.append(t)
+            elif name in nameToTrans:
+                for sameName in nameToTrans[name]:
+                    if abs(sameName['time'] - time) <= 60 and sameName['city'] != city:
+                        ans.append(t)
                         break
 
-                    if city_i != city_j:
-                        invalid_transactions.append(trans_i)
-                        invalid_transactions.append(trans_j)
-
-        return invalid_transactions
+        return ans
