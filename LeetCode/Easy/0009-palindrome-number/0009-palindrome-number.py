071fb4cadec82e76ff314c8c67f559c6fa4c13c9
Jisu Park <86939562+mijisu0103@users.noreply.github.com>
2026-05-03 21:24:32 +0900

    Time: 15 ms (35.52%), Space: 12.3 MB (54.59%) - LeetHub

--- LeetCode/Easy/0009-palindrome-number/0009-palindrome-number.py
+++ LeetCode/Easy/0009-palindrome-number/0009-palindrome-number.py
@@ -1,13 +1,16 @@
-class Solution:
-    def isPalindrome(self, x: int) -> bool:
-        if x < 0:
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+
+        if x < 0 or (x % 10 == 0 and x != 0):
             return False
-
-        reverted = 0
-        original = x
-
-        while original:
-            reverted = reverted * 10 + original % 10
-            original //= 10
         
-        return reverted == x
+        reversed_half = 0
+        while x > reversed_half:
+            reversed_half = reversed_half * 10 + x % 10
+            x //= 10
+        
+        return x == reversed_half or x == reversed_half // 10
