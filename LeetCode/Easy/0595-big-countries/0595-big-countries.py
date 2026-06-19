19dc05652350cfb91003020d39de0acef1d49663
Jisu Park <86939562+mijisu0103@users.noreply.github.com>
2026-06-19 18:18:38 +0900

    Time: 277 ms (38.01%), Space: 68.5 MB (85.59%) - LeetHub

--- LeetCode/Easy/0595-big-countries/0595-big-countries.py
+++ LeetCode/Easy/0595-big-countries/0595-big-countries.py
@@ -2,4 +2,4 @@ import pandas as pd
 
 def big_countries(world: pd.DataFrame) -> pd.DataFrame:
     
-    return world[(world['area'] >= 3000000) | (world['population'] >= 25000000)][['name', 'population', 'area']]
+    return world.loc[(world['area'] >= 3000000) | (world['population'] >= 25000000), ['name', 'population', 'area']]
