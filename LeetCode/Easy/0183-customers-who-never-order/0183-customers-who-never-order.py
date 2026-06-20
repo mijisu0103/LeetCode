72a355b72a9e5a1acb1d0896635c69197e476836
Jisu Park <86939562+mijisu0103@users.noreply.github.com>
2026-06-20 22:10:34 +0900

    Time: 267 ms (86.87%), Space: 67.5 MB (57.51%) - LeetHub

---
+++ LeetCode/Easy/0183-customers-who-never-order/0183-customers-who-never-order.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+def find_customers(customers: pd.DataFrame, orders: pd.DataFrame) -> pd.DataFrame:
+    df = customers[~customers['id'].isin(orders['customerId'])]
+    return df[['name']].rename(columns={'name':'Customers'})
